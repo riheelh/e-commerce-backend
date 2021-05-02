a0fd38cf805d2d8a1f5f9298306c50e323f5369d
Riheel Hamoande <riheel83@gmail.com>
2021-05-01 21:56:44 -0500

    finished product routes

--- models/Product.js
+++ models/Product.js
@@ -21,7 +21,7 @@ Product.init(
       allowNull: false
     },
     price: {
-      type: DataTypes.DECIMAL(10, 2),
+      type: DataTypes.DECIMAL,
       validate: {
         isDecimal: true,
       },
